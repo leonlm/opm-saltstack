be1276fe3c4c12d032404fcf14d5dd70b93db532
leon <sanshuei@126.com>
2018-11-22 23:02:10 +0800

    update

--- opm_saltstack/utils.py
+++ opm_saltstack/utils.py
@@ -2,7 +2,6 @@
 from __future__ import unicode_literals
 from models import *
 from serializers import *
-from django.conf import settings
 
 
 def get_serializer_class():
@@ -28,52 +27,3 @@ def save(method, request_data, **kwargs):
         retdict = {"status": 0, "data": "", "msg": "ERROR,"+json.dumps(serializer.errors)}
     return retdict
 
-
-class SaltStackApi(object):
-    def __init__(self, type):
-        self.saltapi_url = settings.SALTSTACK['URL']
-        self.username = settings.SALTSTACK['USERNAME']
-        self.password = settings.SALTSTACK['PASSWORD']
-        self.eauth = settings.SALTSTACK['EAUTH']
-        self.type = type
-        self.token = self._get_token()
-
-    def _get_token(self):
-        url = self.saltapi_url + "/login"
-        headers = {'Accept':'application/json'}
-        params = {
-            'username': self.username,
-            'password': self.password,
-            'eauth': self.eauth
-        }
-        response = self._salt_api(url, headers, params)
-        return response['token']
-
-    def _salt_api(self, url, headers, data, verify=False, timeout=10):
-        response = requests.post(url, headers=headers, data=data, verify=verify, timeout=timeout)
-        response = json.loads(response.text)
-        return response['return'][0]
-
-    def build(self):
-        if self.type == "async":
-            self._async_run()
-        elif self.type == "cmd":
-            self._cmd_run()
-        else:
-            pass
-            
-    def _cmd_run(self, tgt='*', expr_form='glob', arg='uptime', timeout=10, **kwargs):
-        url = self.saltapi_url
-        headers = {'Accept':'application/json', 'X-Auth-Token':self.token}
-        params = {
-            'client':'local',
-            'tgt': tgt,
-            'expr_form':expr_form,
-            'fun': 'cmd.run',
-            'arg':arg
-        }
-
-        return self._salt_api(url, headers, params)
-
-    def _async_run(self):
-        return True
